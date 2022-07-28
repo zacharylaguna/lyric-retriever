244001499a3c2a69637fb97bacbecf2a83650874
Zachary Laguna <lagunazachary@gmail.com>
2022-07-28 04:17:32 -0500

    prepare for aws

---
+++ deprecated-multiproc.py
@@ -0,0 +1,10 @@
+import multiprocessing.dummy as mp 
+
+def do_print(s):
+    print (s)
+
+if __name__=="__main__":
+    p=mp.Pool(4)
+    p.map(do_print,range(0,10)) # range(0,1000) if you want to replicate your example
+    p.close()
+    p.join()
--- templates/README.md
+++ templates/README.md
@@ -1,3 +1,2 @@
 # templates 
 This folder will contain all of our HTML files, since the Flask framework calls ```render_template()``` to look for index.html in the templates folder.
-
